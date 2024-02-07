87ac338954a70bbb05e4a64ec9c3888cd33b035b
Sávio Moraes <smmsavio@hotmail.com>
2024-02-07 20:20:39 -0300

    feat: add features to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
 import { Outlet } from 'react-router-dom';
-import Navbar from './Components/Navbar';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -24,13 +25,7 @@ const Home = () => {
       <h2 className="title">Melhores Filmes:</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 && topMovies.map((movie) => (
-          <div key={movie.id} className="movie">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-            <h3>{movie.title}</h3>
-            <p>{movie.vote_average}</p>
-          </div>
-        ))}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard movie={movie} key={movie} />)}
       </div>
     </div>
   );
